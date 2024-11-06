4a3aff0e28c0c7ce882898e723de17ef083e7d35
benvigano <beniamino.vigano@protonmail.com>
2024-11-06 07:20:12 +0000

    Register models for admin console

--- visualfolio/main/admin.py
+++ visualfolio/main/admin.py
@@ -1,3 +1,7 @@
+from django.apps import apps
 from django.contrib import admin
 
-# Register your models here.
+app_models = apps.get_app_config('main').get_models()
+
+for model in app_models:
+    admin.site.register(model)
